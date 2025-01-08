29f2d7ddf256d9d2db1a9900774f377e0de77fb5
Vitor Marvulle <marvullevitor@gmail.com>
2025-01-08 16:44:53 -0300

    add: home/sign in/ sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password validations: false
   has_many :sessions, dependent: :destroy
 
-  normalizes :email_address, with: ->(e) { e.strip.downcase }
+  validates :email_address, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
