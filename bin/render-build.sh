b6771402e4efa95e50ca754b3f5761c9566c3913
Vitor Marvulle <marvullevitor@gmail.com>
2025-01-13 19:13:29 -0300

    render repair

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,21 +1,8 @@
-#!/usr/bin/env bash
+# !/usr/bin/env bash# exit on error
 
-# Fail on errors
-set -e
+set -o errexit
 
-# Echo commands to the terminal output
-set -x
+bundle install
+bundle exec rails assets:precompile
 
-# Configurar permissões para scripts
-chmod +x bin/*
-
-# Instalar dependências, ignorando grupos desnecessários
-bundle install --without development test
-
-# Pré-compilar assets para produção
-echo "Precompiling assets..."
-bin/rails assets:precompile || echo "Asset precompilation failed. Skipping."
-
-# Iniciar o servidor Rails
-echo "Starting Rails server..."
-bin/rails server -b 0.0.0.0 -p $PORT
+bundle exec rails assets:clean
